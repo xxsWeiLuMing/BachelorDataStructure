08d49b819bb0eb8cdd752fd6a8cbd9e2ab1046e6
韦前程 <small.students.loming@outlook.com>
2025-01-20 21:33:11 +0800

    顺序表测试

--- DataStructureOnGit/main.cpp
+++ DataStructureOnGit/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main()
 {
 	SeqList L;
-	int a[10] = { 0,1,2,3,4,5,6,7,8,9 };
+	char a[10] = { '0','1','2','3','4','5','6','7','8','9' };
 
 	Seq_InitList(L);
 	Seq_CreateList(L, a, 10);
