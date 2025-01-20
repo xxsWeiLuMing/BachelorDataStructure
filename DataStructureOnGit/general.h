154be73d7fc3354231fdce7524b82d750a9104ef
韦前程 <small.students.loming@outlook.com>
2025-01-20 21:11:09 +0800

    创建总头文件，在其中定义状态代码

---
+++ DataStructureOnGit/general.h
@@ -0,0 +1,11 @@
+#pragma once
+
+//函数结果状态代码
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INFEASIBLE -1
+#define OVERFLOW -2
+
+typedef int Status;//Status是函数的类型，其值是函数结果状态代码
