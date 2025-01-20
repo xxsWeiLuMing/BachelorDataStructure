269c160ccf281a341fab1dacba7364e91d8d8be2
韦前程 <small.students.loming@outlook.com>
2025-01-20 21:28:37 +0800

    顺序表测试

--- DataStructureOnGit/main.cpp
+++ DataStructureOnGit/main.cpp
@@ -1,10 +1,20 @@
 #include<iostream>
 #include<malloc.h>
+using namespace std;
 
 #include"general.h"
 #include"SeqList.h"
 
 int main()
 {
-	
+	SeqList L;
+	int a[10] = { 0,1,2,3,4,5,6,7,8,9 };
+
+	Seq_InitList(L);
+	Seq_CreateList(L, a, 10);
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << L.elem[i];
+	}
 }
