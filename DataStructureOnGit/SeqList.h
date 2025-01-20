10d40ec7499c6f3729ebcd7952981f713c7d0552
韦前程 <small.students.loming@outlook.com>
2025-01-20 21:41:33 +0800

    修改行间距

--- DataStructureOnGit/SeqList.h
+++ DataStructureOnGit/SeqList.h
@@ -18,16 +18,21 @@ Status Seq_InitList(SeqList& L)
 {
     L.elem = (ElemType*)malloc(
         LIST_INIT_SIZE * sizeof(ElemType));
+
     if (!L.elem) exit(OVERFLOW);
+
     L.length = 0;
     L.listsize = LIST_INIT_SIZE;
+
     return OK;
 }
 
 Status Seq_CreateList(SeqList& L, ElemType a[], int n)
 {
     if (!L.elem) exit(ERROR);
+
     for (int i = 0; i <= n; i++)L.elem[i] = a[i];
+
     L.length = n;
     L.listsize = LIST_INIT_SIZE;
 
