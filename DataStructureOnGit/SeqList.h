6b7aefdbb27bd19e1fd71e4ad3e92a283f440a5d
韦前程 <small.students.loming@outlook.com>
2025-01-21 20:02:40 +0800

    重构Seqlist,从struct改为class

--- DataStructureOnGit/SeqList.h
+++ DataStructureOnGit/SeqList.h
@@ -6,71 +6,71 @@ typedef char ElemType;
 #define  LIST_INIT_SIZE  100 //存储空间的初始分配量
 #define  LISTINCREMENT  10 //存储空间的分配增量
 
-typedef  struct SeqList {
+class Seqlist
+{
+public:
     ElemType* elem; //存储空间基址
     int length;     //当前长度
     int listsize;   //当前分配的存储容量
-} SeqList;
 
+    Status Init()
+    {
+        this->elem = (ElemType*)malloc(
+            LIST_INIT_SIZE * sizeof(ElemType));
 
+        if (!this->elem) exit(OVERFLOW);
 
-Status Seq_InitList(SeqList& L)
-{
-    L.elem = (ElemType*)malloc(
-        LIST_INIT_SIZE * sizeof(ElemType));
+        this->length = 0;
+        this->listsize = LIST_INIT_SIZE;
 
-    if (!L.elem) exit(OVERFLOW);
+        return OK;
+    }
 
-    L.length = 0;
-    L.listsize = LIST_INIT_SIZE;
+    Status Create(ElemType a[], int n)
+    {
+        if (!this->elem) exit(ERROR);
 
-    return OK;
-}
+        for (int i = 0; i <= n; i++)this->elem[i] = a[i];
 
-Status Seq_CreateList(SeqList& L, ElemType a[], int n)
-{
-    if (!L.elem) exit(ERROR);
+        this->length = n;
+        this->listsize = LIST_INIT_SIZE;
 
-    for (int i = 0; i <= n; i++)L.elem[i] = a[i];
+        return OK;
+    }
 
-    L.length = n;
-    L.listsize = LIST_INIT_SIZE;
+    Status Traverse()
+    {
+        if (this->length == 0)return ERROR;
 
-    return OK;
-}
+        for (int i = 0; i < this->length; i++)
+            cout << this->elem[i] << " ";
 
-Status Seq_ListTraverse(SeqList L)
-{
-    if (L.length==0)return ERROR;
+        cout << endl;
 
-    for (int i = 0; i < L.length; i++)
-        cout << L.elem[i] << " ";
+        return OK;
+    }
 
-    cout << endl;
+    void Destroy()
+    {
+        free(this->elem);
+    }
 
-    return OK;
-}
+    Status IsEmpty()
+    {
+        return (this->length == 0);
+    }
 
-void Seq_DestroyList(SeqList& L) 
-{
-    free(L.elem);
-}
+    int Length()
+    {
+        return(this->length);
+    }
 
-Status Seq_IsListEmpty(SeqList L)
-{
-    return (L.length == 0);
-}
+    Status GetElem(int i, ElemType& e)
+    {
+        if (i<1 || i>this->length)return ERROR;
 
-int Seq_ListLength(SeqList L)
-{
-    return(L.length);
-}
+        e = this->elem[i - 1];//自然语言中没有“第0个”一说，故第i个的编号为i-1
 
-Status Seq_GetElem(SeqList L, int i, ElemType& e)
-{
-    if (i<1 || i>L.length)return ERROR;
-
-    e = L.elem[i - 1];//自然语言中没有“第0个”一说，故第i个的编号为i-1
-
-    return OK;
-}
+        return OK;
+    }
+};
