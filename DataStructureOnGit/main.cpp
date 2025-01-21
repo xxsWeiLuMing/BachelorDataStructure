8d5803815b14e6d25cfc0407d0412d810e836eb3
韦前程 <small.students.loming@outlook.com>
2025-01-21 20:06:18 +0800

    测试class SeqList

--- DataStructureOnGit/main.cpp
+++ DataStructureOnGit/main.cpp
@@ -10,10 +10,11 @@ int main()
 	SeqList L;
 	char a[10] = { '0','1','2','3','4','5','6','7','8','9' };
 
-	Seq_InitList(L);
-	Seq_CreateList(L, a, 10);
+	L.Init();
+	L.Create(a, 10);
+	L.Traverse();
 
 	ElemType e;
-	Seq_GetElem(L, 3, e);
+	L.GetElem(5, e);
 	cout << e << endl;
 }
