6a32c6f03ce27f1c7f2ad561f7ff5ca39a432ded
韦前程 <small.students.loming@outlook.com>
2025-01-24 23:43:09 +0800

    区分test与main

--- DataStructureOnGit/main.cpp
+++ DataStructureOnGit/main.cpp
@@ -3,17 +3,9 @@
 using namespace std;
 
 #include"general.h"
-#include"SeqList.h"
+#include"testSeqList.h"
 
 int main()
 {
-	SeqList L;
-	char a[10] = { '0','1','2','3','4','5','6','7','8','9' };
-
-	Seq_InitList(L);
-	Seq_CreateList(L, a, 10);
-
-	ElemType e;
-	Seq_GetElem(L, 3, e);
-	cout << e << endl;
+	
 }
