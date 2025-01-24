7d65c05d26345b004c07c35c4457939ba810903c
韦前程 <small.students.loming@outlook.com>
2025-01-24 23:38:00 +0800

    在master中使用struct

--- DataStructureOnGit/SeqList.h
+++ DataStructureOnGit/SeqList.h
@@ -75,3 +75,38 @@ Status GetElem(SeqList L, int i, ElemType& e)
 
     return OK;
 }
+
+int LocateElem(SeqList L,ElemType e)
+{
+    for (int i = 0; i < L.length; i++)
+    {
+        if (L.elem[i] == e)return i + 1;
+    }
+
+    return ERROR;
+}
+
+Status Insert(SeqList& L,int i, ElemType e)
+{
+    if (i<1 || i>L.length + 1)return ERROR;
+
+    if (L.length >= L.listsize)
+    {
+        ElemType* newbase = (ElemType*)realloc(L.elem,
+            (L.listsize + LISTINCREMENT) * sizeof(ElemType));
+        if (!newbase) exit(OVERFLOW);
+        L.elem = newbase;
+        L.listsize += LISTINCREMENT;
+    }
+
+    ElemType* q = &(L.elem[i - 1]);
+    for (ElemType* p = &(L.elem[L.length - 1]); p >= q; p--)
+    {
+        *(p + 1) = *p;
+    }
+
+    *q = e;
+    L.length++;
+
+    return OK;
+}
