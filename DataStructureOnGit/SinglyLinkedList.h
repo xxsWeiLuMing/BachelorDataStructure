c1df25cfa340326318f4ca92e524d308a2431a4d
韦前程 <small.students.loming@outlook.com>
2025-01-31 20:09:41 +0800

    备注了要实现的功能

--- DataStructureOnGit/SinglyLinkedList.h
+++ DataStructureOnGit/SinglyLinkedList.h
@@ -6,3 +6,14 @@ typedef  struct  LNode {
     ElemType data;
     struct  LNode* next;
 } LNode, * LinkList;
+
+//初始化
+//创建
+//遍历输出
+//销毁
+//判断空
+//求长度
+//求第i个元素的值
+//求第1个值为e的元素的位序
+//插入元素e到第i个
+//删序第i个元素
