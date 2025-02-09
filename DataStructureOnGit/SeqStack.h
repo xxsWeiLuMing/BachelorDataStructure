ad17c7ab06ffe43bd10d28f9f3f9d1ba3e3335d1
韦前程 <small.students.loming@outlook.com>
2025-02-09 16:42:58 +0800

    注释要实现的函数

--- DataStructureOnGit/SeqStack.h
+++ DataStructureOnGit/SeqStack.h
@@ -4,4 +4,23 @@
 #include<malloc.h>
 using namespace std;
 
+#define  STACK_INIT_SIZE   100 
+#define  STACKINCREMENT  10 
 
+typedef char SElemType;
+
+typedef  struct {
+    SElemType* base;
+    SElemType* top;
+    int   stacksize;
+} SqStack;//顺序栈
+
+//初始化
+//销毁
+//遍历输出
+//判断空
+//求长度
+//得到栈首元素
+//出栈
+//出栈
+//遍历输出
