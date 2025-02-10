7babdfe7807b9a27f1384e0f393bd444f814b519
韦前程 <small.students.loming@outlook.com>
2025-02-11 00:30:00 +0800

    链栈

---
+++ DataStructureOnGit/LinkStack.h
@@ -0,0 +1,28 @@
+#pragma once
+#define  STACK_INIT_SIZE   100 
+#define  STACKINCREMENT  10 
+
+typedef char SElemType;
+
+typedef  struct  SLNode {
+    SElemType   data;
+    struct SLNode* next;
+} SLNode, * SLinkList;
+
+//初始化
+
+//销毁
+
+//遍历输出
+
+//判断空
+
+//求长度
+
+//得到栈首元素
+
+//入栈
+
+//出栈
+
+//清空栈
