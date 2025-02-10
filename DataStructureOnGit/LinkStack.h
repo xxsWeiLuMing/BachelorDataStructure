bb4480c55dbdbadecef8a9871a479c321a817255
韦前程 <small.students.loming@outlook.com>
2025-02-11 00:33:07 +0800

    Copilot生成代码

--- DataStructureOnGit/LinkStack.h
+++ DataStructureOnGit/LinkStack.h
@@ -15,19 +15,73 @@ typedef  struct  SLNode {
 } SLNode, * SLinkList;
 
 //初始化
+void InitStack(SLinkList& S) {
+    S = nullptr;
+}
 
 //销毁
+void DestroyStack(SLinkList& S) {
+    SLNode* p;
+    while (S) {
+        p = S;
+        S = S->next;
+        free(p);
+    }
+}
 
 //遍历输出
+void TraverseStack(SLinkList S) {
+    SLNode* p = S;
+    while (p) {
+        cout << p->data << " ";
+        p = p->next;
+    }
+    cout << endl;
+}
 
 //判断空
+bool StackEmpty(SLinkList S) {
+    return S == nullptr;
+}
 
 //求长度
+int StackLength(SLinkList S) {
+    int length = 0;
+    SLNode* p = S;
+    while (p) {
+        length++;
+        p = p->next;
+    }
+    return length;
+}
 
 //得到栈首元素
+bool GetTop(SLinkList S, SElemType& e) {
+    if (S == nullptr) return false;
+    e = S->data;
+    return true;
+}
 
 //入栈
+void Push(SLinkList& S, SElemType e) {
+    SLNode* p = (SLNode*)malloc(sizeof(SLNode));
+    p->data = e;
+    p->next = S;
+    S = p;
+}
 
 //出栈
+bool Pop(SLinkList& S, SElemType& e) {
+    if (S == nullptr) return false;
+    SLNode* p = S;
+    e = p->data;
+    S = S->next;
+    free(p);
+    return true;
+}
 
 //清空栈
+void ClearStack(SLinkList& S) {
+    DestroyStack(S);
+    S = nullptr;
+}
