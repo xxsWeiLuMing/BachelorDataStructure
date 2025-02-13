9351cd407a9320cdb0004a2f022dede1ac5128cf
韦前程 <small.students.loming@outlook.com>
2025-02-13 22:16:43 +0800

    确定队列基本操作

--- DataStructureOnGit/LinkQueue.h
+++ DataStructureOnGit/LinkQueue.h
@@ -1,1 +1,25 @@
 #pragma once
+#include"general.h"
+#include<iostream>
+#include<malloc.h>
+using namespace std;
+
+
+
+//初始化
+
+//销毁
+
+//清空队列
+
+//判断空
+
+//求长度
+
+//得到队首元素
+
+//入队列
+
+//出队列
+
+//遍历输出
