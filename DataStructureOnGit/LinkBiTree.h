57c598411e8dbd1b5206d1151fabc8c1c9445fd2
韦前程 <small.students.loming@outlook.com>
2025-02-17 21:55:05 +0800

    引用头文件。建树函数CreateBiTree(BiTree& T)

--- DataStructureOnGit/LinkBiTree.h
+++ DataStructureOnGit/LinkBiTree.h
@@ -1,4 +1,8 @@
 #pragma once
+#include"general.h"
+#include<iostream>
+#include<malloc.h>
+using namespace std;
 
 typedef char TElemType;
 
@@ -7,3 +11,20 @@ typedef  struct  BiTNode
     TElemType data;
     struct  BiTNode* lchild, * rchild;
 } BiTNode, * BiTree;
+
+//建树
+Status  CreateBiTree(BiTree& T)//先序输入，创建二叉树
+{
+    char ch;
+    scanf_s("%c", &ch);
+    if (ch == ' ')  T = NULL;
+    else
+    {
+        T = (BiTNode*)malloc(sizeof(BiTNode));
+        if (!T) exit(OVERFLOW);
+        T->data = ch;
+        CreateBiTree(T->lchild);
+        CreateBiTree(T->rchild);
+    }
+    return OK;
+}
