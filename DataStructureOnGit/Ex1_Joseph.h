fcf7b289e1baba7c1aa76183de3c3f2c4a3ac98d
韦前程 <small.students.loming@outlook.com>
2025-03-08 21:54:10 +0800

    用pos[]记录位序

--- DataStructureOnGit/Ex1_Joseph.h
+++ DataStructureOnGit/Ex1_Joseph.h
@@ -2,8 +2,9 @@
 
 #include"CircularLinkedList.h"
 
-void Joseph() {
-    int m, n, a[30];
+void Joseph() 
+{
+    int m, n;
 
     cout << "输入报数上限m，然后输入人数n（n<30）:";
     cin >> m >> n;
@@ -14,4 +15,30 @@ void Joseph() {
     cout << "输入" << n << "个人的密码:";
     CreateList(L, n);
     //TraverseList(L);
+
+    CirLinkList p = L->next;
+    int pos[30];
+    for (int i = 1; i <= n; i++)
+    {
+        pos[p->data - '0'] = i;
+        p = p->next;
+    }
+
+    p = L->next;
+    while (L->next)
+    {
+        
+        for (int i = m; i > 0; i--)
+        {
+            p = p->next;
+        }
+            
+        cout << pos[p->data-'0'] << " ";
+
+        ElemType e;
+        p = p->next;
+        ListDelete(L, m % n, e);
+        m = e-'0';
+        n--;
+    }
 }
