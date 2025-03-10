d9bb4a8e497b43f594b5032f0f043f19ac8ce19b
韦前程 <small.students.loming@outlook.com>
2025-03-10 19:57:27 +0800

    HuffmanTree数据结构定义

---
+++ DataStructureOnGit/HuffmanTree.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include"general.h"
+#include<iostream>
+#include<malloc.h>
+using namespace std;
+
+#define codebit 10
+typedef  struct {
+    unsigned int    weight;//权
+    unsigned int    parent, lchild, rchild;//父母，左孩子，右孩子
+    char c;//编码系统的字符
+} HTNode, * HuffmanTree;  //动态分配数组存储哈夫曼树
+
+typedef  char** HuffmanCode;  //动态分配数组存储哈夫曼编码表
