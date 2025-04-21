ef5535882623d6faa0879f9c697be4df6640d01e
韦前程 <small.students.loming@outlook.com>
2025-04-21 17:43:44 +0800

    降低cmake_minimum_required版本为3.30

--- cmake-build-debug/CMakeFiles/BachelorDataStructure.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/BachelorDataStructure.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/cygdrive/d/ApP/Programming/CLionProjects/FormGit/BachelorDataStructure/main.cpp" "CMakeFiles/BachelorDataStructure.dir/main.cpp.o" "gcc" "CMakeFiles/BachelorDataStructure.dir/main.cpp.o.d"
+  "/cygdrive/d/ApP/Programming/CLionProjects/BachelorDataStructure/main.cpp" "CMakeFiles/BachelorDataStructure.dir/main.cpp.o" "gcc" "CMakeFiles/BachelorDataStructure.dir/main.cpp.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
