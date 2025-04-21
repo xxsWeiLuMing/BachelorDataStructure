cc131d375027f5ad38fe2bf16c8037f7ff0b80d8
韦前程 <small.students.loming@outlook.com>
2025-04-21 15:05:28 +0800

    clion生成的文件

---
+++ cmake-build-debug/CMakeFiles/BachelorDataStructure.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BachelorDataStructure.exe"
+  "BachelorDataStructure.pdb"
+  "CMakeFiles/BachelorDataStructure.dir/main.cpp.o"
+  "CMakeFiles/BachelorDataStructure.dir/main.cpp.o.d"
+  "libBachelorDataStructure.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BachelorDataStructure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- cmake-build-debug/CMakeFiles/rebuild_cache.dir/utility.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-cd /cygdrive/d/ApP/Programming/CLionProjects/BachelorDataStructure/cmake-build-debug
-/cygdrive/c/Users/small/AppData/Local/JetBrains/CLion2025.1/cygwin_cmake/bin/cmake.exe --regenerate-during-build -S$(CMAKE_SOURCE_DIR) -B$(CMAKE_BINARY_DIR)
