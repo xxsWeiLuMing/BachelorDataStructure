0749f5257e1be9121f724e04af63a6f0fcb0b305
韦前程 <small.students.loming@outlook.com>
2025-05-03 15:43:56 +0800

    准备编码

---
+++ Source/DataStructure/Tree/HuffmanTree/2_Code.h
@@ -0,0 +1,8 @@
+//
+// Created by small on 2025/5/3.
+//
+
+#ifndef INC_2_CODE_H
+#define INC_2_CODE_H
+
+#endif //INC_2_CODE_H
---
+++ Source/DataStructure/Tree/HuffmanTree/tool.h
@@ -0,0 +1,8 @@
+//
+// Created by small on 2025/5/3.
+//
+
+#ifndef TOOL_H
+#define TOOL_H
+
+#endif //TOOL_H
